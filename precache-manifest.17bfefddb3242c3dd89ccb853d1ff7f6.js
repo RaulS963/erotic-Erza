bc6fcb211057f26ca67b461a0ddf43676ca73882
Anindya Das <dasanindya9809@gmail.com>
2020-03-22 20:49:10 +0530

    Updates

---
+++ precache-manifest.17bfefddb3242c3dd89ccb853d1ff7f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52f4e036b0839405e1ee9dba88b44ada",
+    "url": "/erotic-Erza/index.html"
+  },
+  {
+    "revision": "8c3f207e939a858ad1ad",
+    "url": "/erotic-Erza/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "df73aa759f1cb513e9c1",
+    "url": "/erotic-Erza/static/js/2.5ab1fea5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/erotic-Erza/static/js/2.5ab1fea5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c3f207e939a858ad1ad",
+    "url": "/erotic-Erza/static/js/main.c5bb7848.chunk.js"
+  },
+  {
+    "revision": "bd933e2048cf6db0599e",
+    "url": "/erotic-Erza/static/js/runtime-main.db8cf3e5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/erotic-Erza/static/media/logo.5d5d9eef.svg"
+  }
+]);
